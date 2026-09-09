c4e44b0c96fc5d4dbebb881699ecd6d8425529cd
Siddhant Shukla <shuklasiddhant302@gmail.com>
2026-09-09 06:21:49 +0530

    Introduction

--- Day_01/src/first.mjs
+++ Day_01/src/first.mjs
@@ -10,3 +10,9 @@ console.log("Hello ji");
 // If we want to use MJS module:
 // 1) use .mjs extension instead of .js extension
 // 2) use package,json file in which we write "type": "module"
+
+// CJS                          |           MJS
+// Older way                    |           Newer way
+// require, module.exports      |           import, export
+// synchronous                  |           asynchronous
+// non-strict                   |           strict
